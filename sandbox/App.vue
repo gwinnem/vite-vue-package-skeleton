67342a3f5fcef3ad61101c62efa99e6ea1954944
Geirr Winnem <g.winnem@ringana.com>
2022-12-07 12:48:53 +0100

    Initial commit

---
+++ sandbox/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <main>
+    <h1>Welcome to your your sandbox environment</h1>
+    <VueComponentNpmExample />
+  </main>
+</template>
+
+<script lang="ts" setup>
+  import VueComponentNpmExample from '../src/lib-compponents/Test.vue';
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/lib-compponents/Test.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  defineProps({
+        msg: { type: String }
+      }
+  );
+
+  const count = ref(0);
+</script>
+
+<style scoped>
+h1 {
+  color: #7e22ce;
+}
+</style>
