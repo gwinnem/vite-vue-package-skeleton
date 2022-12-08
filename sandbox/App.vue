506059797e0f33f577e0f8019f3dd048270e2fb5
Geirr Winnem <g.winnem@ringana.com>
2022-12-08 06:48:38 +0100

    Added new test component and renamed one

--- sandbox/App.vue
+++ sandbox/App.vue
@@ -2,12 +2,23 @@
   <main>
     <h1>Welcome to your your sandbox environment</h1>
     <TestComponent />
+    <br/>
+    <Input
+      label="firstname"
+      :input-attributes="{
+        placeholder: 'Enter your firstname...',
+        type: 'text',
+        autocomplete: 'given-name',
+      }"
+    >
+      First-name
+    </Input>
   </main>
 </template>
 
 <script lang="ts" setup>
-  import TestComponent from '../src/lib/Test.vue';
-
+  import TestComponent from '../src/lib/ButtonComponent.vue';
+  import Input from '../src/lib/InputComponent.vue';
 </script>
 
 <style lang="scss" scoped></style>
